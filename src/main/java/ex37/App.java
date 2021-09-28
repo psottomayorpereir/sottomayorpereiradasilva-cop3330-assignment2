5e57e2b0ad21af7ee5fa0a598e51d14ffa9bda0a
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-27 23:46:19 -0700

    Updated ex37

--- src/main/java/ex37/App.java
+++ src/main/java/ex37/App.java
@@ -2,42 +2,54 @@
  *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Pedro Henrique Sotto-Mayor Pereira da Silva
  */
-package ex35;
+package ex37;
 
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class App
-{
+public class App {
 
-    public static void main( String[] args )
-    {
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        ArrayList<String> list = new ArrayList<String>();
-        int len=0, spec=0, num=0, total=0;
+        ArrayList<Character> list = new ArrayList<>();
+        int len = 0, spec = 0, num = 0, total = 0;
         String upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerLetters = "abcdefghijklmnopqrstuvwxyz";
         String specChars = "!@#$";
         String numbers = "1234567890";
         char c;
+        int number = 0;
 
         System.out.print("What is the minimum length? ");
-        len=sc.nextInt();
+        len = sc.nextInt();
         System.out.print("How many special characters? ");
-        spec=sc.nextInt();
+        spec = sc.nextInt();
         System.out.print("How many numbers? ");
-        num=sc.nextInt();
+        num = sc.nextInt();
 
-        total=len+spec+num;
-        while(total!=0){
-            while(len>0){
-
+        total = len + spec + num;
+        int temp=total;
+        while (temp != 0) {
+            number = rand.nextInt(4);
+            if (number == 0) {
+                c = upperLetters.charAt(rand.nextInt(upperLetters.length()));
+            } else if (number == 1) {
+                c = lowerLetters.charAt(rand.nextInt(lowerLetters.length()));
+            } else if (number == 2) {
+                c = specChars.charAt(rand.nextInt(specChars.length()));
+            } else {
+                c = numbers.charAt(rand.nextInt(numbers.length()));
             }
             list.add(c);
+            temp=temp-1;
         }
 
-        System.out.print("Your password is "+ list);
+        System.out.print("Your password is ");
+        for(int j=0;j<total;j++){
+            System.out.print(list.get(j));
+        }
 
+    }
 }
